d83d9666fb63035924e5e0f8ad9db659883d3d0b
Caleb Jacobs <cajacobs@mtu.edu>
2019-07-31 13:24:56 -0400

    Added interpolant calculations and derivatives, the S_xij may need to be worked on

--- Tensors_3D/RBFT-FD.jl
+++ Tensors_3D/RBFT-FD.jl
@@ -142,7 +142,7 @@ end
 # Degree array generator
 function polyMat(vars, deg)
     if deg < 0
-        return []
+        return Array{Int64}(undef,vars,0)
     else
         tmp::Array{Int64,1} = zeros(vars);
         array::Array{Array{Int64,1},1} = [copy(tmp)];
@@ -170,7 +170,7 @@ end
 
 ## Finite difference functions
 # Collocation matrix generator
-function colloc(nodes, poly, m)
+function colloc(nodes, poly = Array{Int64}(undef,0,0); m = 3)
     # Number of nodes in cluster
     N = size(nodes,2);
     # Number of dimensions
@@ -181,18 +181,22 @@ function colloc(nodes, poly, m)
     # Preallocating space
     A11 = zeros(N,N);
     A12 = zeros(N,P);
-    
+
+    # Add PHS contributions
     for j ∈ 1:N, i ∈ 1:N
         A11[i,j] = ϕ(nodes[:,j],nodes[:,i],m);
     end
-    
-    for j ∈ 1:P, i ∈ 1:N
-        tmp = 1;
-        for k ∈ 1:D
-            tmp *= nodes[k,i]^poly[k,j];
+
+    # Add polynomial contributions
+    if poly != []
+        for j ∈ 1:P, i ∈ 1:N
+            tmp = 1;
+            for k ∈ 1:D
+                tmp *= nodes[k,i]^poly[k,j];
+            end
+            
+            A12[i,j] = tmp;
         end
-        
-        A12[i,j] = tmp;
     end
 
     # Construst A
@@ -200,4 +204,187 @@ function colloc(nodes, poly, m)
          A12' zeros(P,P)];
     
     return A 
+end
+
+# Routine for computing RBF-PHS interpolant weights
+function findλ(nodes, f, poly = Array{Int64}(undef,0,0); m = 3)
+    # Number of nodes
+    N = size(f,1);
+    # Number of dimensions
+    D = size(nodes,1);
+    # Number of polynomial terms
+    P = size(poly,2);
+
+    # Construct collocation matrix
+    A = colloc(nodes, poly, m = m);
+
+    # Construct b in Aλ=b
+    b = [f;zeros(P)];
+
+    # Solve for the weights
+    λ = A\b;
+
+    return λ
+end
+
+# Compute interpolant at X_c with respect to xi
+function S(nodes, Xc, λ, poly = Array{Int64}(undef,size(nodes,1),0); m = 3)
+    # Number of nodes
+    N = size(nodes,2);
+    # Number of dimensions
+    D = size(nodes,1);
+    # Number of polynomial terms
+    P = size(poly,2);
+
+    # Add PHS contribution
+    tmp = 0;
+    for i ∈ 1:N
+        tmp += λ[i]*ϕ(Xc,nodes[:,i],m);
+    end
+
+    # Add polynomial contribution
+    for i ∈ 1:P
+        tmp2 = λ[i+N];
+        for j ∈ 1:D
+            tmp2 *= Xc[j]^poly[j,i];
+        end
+        tmp += tmp2
+    end
+
+    return tmp
+end
+
+# Compute derivative of interpolant at X_c with respect to xi
+function S_xi(nodes, Xc, λ, ii, poly = Array{Int64}(undef,size(nodes,1),0); m = 3)
+    # Number of nodes
+    N = size(nodes,2);
+    # Number of dimensions
+    D = size(nodes,1);
+    # Number of polynomial terms
+    P = size(poly,2);
+
+    # Add PHS contribution
+    tmp = 0;
+    for i ∈ 1:N
+        tmp += λ[i]*ϕ_xi(Xc,nodes[:,i],m,ii);
+    end
+    
+    # Add polynomial contribution
+    for i ∈ 1:P
+        p = poly[ii,i]
+        tmp2 = λ[i+N]*p;
+        for j ∈ 1:ii-1
+            tmp2 *= Xc[j]^poly[j,i]
+        end
+
+        # Add derivative component of polynomial
+        p = p-1;
+        if p >= 0
+            tmp2 *= Xc[ii]^p;
+        end
+        
+        for j ∈ ii+1:D
+            tmp2 *= Xc[j]^poly[j,i]
+        end
+        
+        tmp += tmp2
+    end
+
+    return tmp
+end
+
+# Compute derivative of interpolant at X_c with respect to xi
+function S_xii(nodes, Xc, λ, ii, poly = Array{Int64}(undef,size(nodes,1),0); m = 3)
+    # Number of nodes
+    N = size(nodes,2);
+    # Number of dimensions
+    D = size(nodes,1);
+    # Number of polynomial terms
+    P = size(poly,2);
+
+    # Add PHS contribution
+    tmp = 0;
+    for i ∈ 1:N
+        tmp += λ[i]*ϕ_xii(Xc,nodes[:,i],m,ii);
+    end
+    
+    # Add polynomial contribution
+    for i ∈ 1:P
+        p = poly[ii,i];
+        tmp2 = λ[i+N]*p*(p-1);
+
+        # Add standard polynomial terms
+        for j ∈ 1:ii-1
+            tmp2 *= Xc[j]^poly[j,i]
+        end
+
+        # Add derivative component of polynomial
+        p = p-2;
+        if p >= 0
+            tmp2 *= Xc[ii]^p;
+        end
+
+        # Add more standard polynomial terms
+        for j ∈ ii+1:D
+            tmp2 *= Xc[j]^poly[j,i]
+        end
+        
+        tmp += tmp2
+    end
+
+    return tmp
+end
+
+# Compute derivative of interpolant at X_c with respect to xi
+function S_xij(nodes, Xc, λ, ii, jj, poly = Array{Int64}(undef,size(nodes,1),0); m = 3)
+    # Number of nodes
+    N = size(nodes,2);
+    # Number of dimensions
+    D = size(nodes,1);
+    # Number of polynomial terms
+    P = size(poly,2);
+
+    # Add PHS contribution
+    tmp = 0;
+    for i ∈ 1:N
+        tmp += λ[i]*ϕ_xii(Xc,nodes[:,i],m,ii);
+    end
+    
+    # Add polynomial contribution
+    for i ∈ 1:P
+        (k1,k2) = (ii<jj ? (ii,jj) : (jj,ii));
+        p1 = poly[k1,i];
+        p2 = poly[k2,i];
+        tmp2 = λ[i+N]*p1*p2;
+        
+        # Add standard polynomial terms
+        for j ∈ 1:k1-1
+            tmp2 *= Xc[j]^poly[j,i]
+        end
+        
+        # Add derivative component of polynomial
+        p1 = p-1;
+        if p >= 0
+            tmp2 *= Xc[ii]^p1;
+        end
+        
+        for j ∈ k1+1:k2-1
+            tmp2 *= Xc[j]^poly[j,i]
+        end
+        
+        # Add derivative component of polynomial
+        p2 = p-1;
+        if p >= 0
+            tmp2 *= Xc[ii]^p2;
+        end
+
+        # Add more standard polynomial terms
+        for j ∈ k2+1:D
+            tmp2 *= Xc[j]^poly[j,i]
+        end
+        
+        tmp += tmp2
+    end
+
+    return tmp
 end
