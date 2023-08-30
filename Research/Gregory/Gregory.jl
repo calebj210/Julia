a8c6a0f96ba435fccfef3996cab3adf6f5d14dc7
Caleb Jacobs <calebjacobs210@gmail.com>
2023-08-30 15:54:16 -0600

    Gregory progress

--- Research/Gregory/Gregory.jl
+++ Research/Gregory/Gregory.jl
@@ -1,12 +1,13 @@
 #=
-# Simple gregory integration
+# Generalized Gregory Integration
 #
+# Author: Caleb Jacobs
+# DLM: August 30, 2023
 =#
 
 using SpecialFunctions
 using LinearAlgebra
 using FFTW
-using Plots
 
 """
     vand(z⃗)
@@ -29,11 +30,61 @@ function ζ⃗(α, h, z⃗)
 end
 
 """
+    getCorrections(α, h, z⃗)
+Compute trapezoidal rule end corrections given `z⃗` is a list of the end correction nodes relative to the base point, `α` is the order of the singularity
+"""
+function getCorrections(α, h, z⃗)
+    w = vand(z⃗) \ ζ⃗(α, h, z⃗)
+
+    return w
+end
+
+"""
+    nonSingWeights(h, z⃗)
+Compute singularity free end corrections given spacing `h` and correction nodes `z⃗`.
+"""
+function nonSingWeights(h, z⃗)
+    w = getCorrections(-1, h, z⃗)   # Generate most of the weights
+    w[1] -= 1                       # Fix first weight
+
+    return w
+end
+
+"""
+    nonSingInt(f, a, b, n, p)
+Integrate a function `f` from `a` to `b` with no singularities using `n` nodes and order `p`
+"""
+function nonSingGregoryInt(f, a, b, n, p)
+    z⃗ = [range(a, b, length = n)...]                # Trapezoidal nodes
+    h = z⃗[2] - z⃗[1]                                 # Step size
+
+    w = ones(n)                                     # Uncorrected TR weights
+    if p >= 2                                       # Compute corrections if needed
+        c = nonSingWeights(h, z⃗[1 : p - 1] .- a)
+        w[1 : p - 1] += c                           # Correct left weights
+        w[n : -1 : n - p + 2] += c                  # Correct right weights
+    end 
+    w *= h                                          # Rescale weights to step size
+
+    return w ⋅ f.(z⃗)                                # Compute integral
+end
+
+"""
+    getNodes(N, r)
+Generate `N` equispaced nodes on a complex circle of radius `r`.
+"""
+function getNodes(N::Integer, r)
+    z⃗ = [r * cispi(2i / N) for i ∈ 0 : N - 1]      # Compute scaled roots of unitny
+
+    return z⃗
+end
+
+"""
     dftζ⃗(α, h, r, N)
 Construct DFT compatible zeta vector for end corrected system using `N` roots of unity scaled to have radii `r`.
 """
 function dftζ⃗(α, h, r, N)
-    RHS = [-zeta(α + 1 - i) * (h / r)^i for i ∈ 0 : N - 1]
+    RHS = [-zeta(α - i) * (h / r)^i for i ∈ 0 : N - 1]
 
     return RHS
 end
@@ -49,59 +100,8 @@ function getDFTCorrections(α, h, r, N)
 end
 
 """
-    getCorrections(α, h, z⃗)
-Compute trapezoidal rule end corrections given `z⃗` is a list of the end correction nodes relative to the base point, `α` is the order of the singularity
-"""
-function getCorrections(α, h, z⃗)
-    w = vand(z⃗) \ ζ⃗(α, h, z⃗)
-
-    return w
-end
-
-"""
-    getNodes(N, r)
-Generate `N` equispaced nodes on a complex circle of radius `r`.
-"""
-function getNodes(N::Integer, r)
-    z⃗ = [r * cispi(-2i / N) for i ∈ 0 : N - 1]      # Compute scaled roots of unitny
-
-    return z⃗
-end
-
-"""
-    nonSingWeights(h, z⃗)
-Compute singularity free end corrections given spacing `h` and correction nodes `z⃗`.
-"""
-function nonSingWeights(h, z⃗)
-    w = -getCorrections(-1, h, z⃗)   # Generate most of the weights
-    w[1] -= 1                       # Fix first weight
-
-    return w
-end
-
-"""
-    nonSingInt(f, a, b, n, p)
-Integrate a function `f` from `a` to `b` with no singularities using `n` nodes and order `p`
-"""
-function nonSingInt(f, a, b, n, p)
-    z⃗ = [range(a, b, length = n)...]                # Trapezoidal nodes
-    h = z⃗[2] - z⃗[1]                                 # Step size
-
-    w = ones(n)                                     # Uncorrected TR weights
-    if p >= 2                                       # Compute corrections if needed
-        c = nonSingWeights(h, z⃗[1 : p - 1] .- a)
-        w[1 : p - 1] += c                           # Correct left weights
-        w[n : -1 : n - p + 2] += c                  # Correct right weights
-    end 
-    w *= h                                          # Rescale weights to step size
-
-    return w ⋅ f.(z⃗)                                # Compute integral
-end
-
-
-"""
     nonSingDFTInt(f, a, b, n, r, N)
-Integrate a function `f` from `a` to `b` with no singularities using `n` internal nodes and `N` correction nodes a radius of `r` away from the base
+Integrate a function `f` from `a` to `b` with no singularities using `n` internal nodes and `N` correction nodes a radius of `r` away from the base.
 """
 function nonSingDFTInt(f, a, b, n, N, r)
     z⃗Int = [range(a, b, length = n)...]         # Internal nodes
@@ -111,7 +111,7 @@ function nonSingDFTInt(f, a, b, n, N, r)
     fInt   =  h * sum(f.(z⃗Int[2 : end - 1]))    # Internal trapezoidal rule
 
     if N > 0
-        w = getDFTCorrections(-1, h, r*h, N)    # Correction weights
+        w = getDFTCorrections(0, h, r*h, N)     # Correction weights
         fLeft  = h * w ⋅ f.( z⃗End .+ a)         # Left end correction
         fRight = h * w ⋅ f.(-z⃗End .+ b)         # Right end correction
     else
@@ -124,23 +124,31 @@ function nonSingDFTInt(f, a, b, n, N, r)
     return ∫fdx
 end
 
-function convergencePlot(r = 5)
-    a = 0 
-    b = 1
-    f(x) = sin(x^2)^2
-    tru  = 0.1662015759264158
-    ns   = [10:1:1000...]
-    errs = zeros(length(ns))
+"""
+    singDFTInt(f, a, b, n, r, N)
+Integrate a function `f`/(x - `a`)`ᵅ` from `a` to `b` using `n` internal nodes and `N` correction nodes a radius of `r` away from the base.
+"""
+function singDFTInt(f, a, b, n, N, r, α)
+    z⃗Int = [range(a, b, length = n)...]                         # Internal nodes
+    h = z⃗Int[2] - z⃗Int[1]                                       # Internal spacing
+    z⃗Ends = getNodes(N, r * h)                                  # Correction nodes
 
-    p = plot(ns[[1,end]], 1 ./ ns[[1,end]], lw = 5, ls = :dash)
-    plot!(ns[[1,end]], 1 ./ ns[[1,end]].^2, lw = 5, ls = :dash)
-    plot!(ns[[1,end]], 1 ./ ns[[1,end]].^3, lw = 5, ls = :dash)
-    for N ∈ 0 : 1 : 8
-        for i ∈ 1 : length(ns)
-            errs[i] = abs(nonSingDFTInt(f, a, b, ns[i], N, r) - tru)
-        end
-        plot!(ns, errs, yaxis = :log, xaxis = :log, lw = 17 - 2N)
+    sf(x) = f(x) / (x - a)^α                                    # Singular function
+
+    fInt   =  h * sum(sf.(z⃗Int[2 : end - 1]))                   # Internal trapezoidal rule
+
+    if N > 0
+        wL = getDFTCorrections(α, h, r*h, N)                    # Left, singularity correction weights
+        fLeft  = h^(1 - α) * wL ⋅ f.(z⃗Ends .+ a)                # Left end correction
+
+        wR = getDFTCorrections(0, h, r*h, N)                    # Right correction weights
+        fRight = h * wR ⋅ sf.(-z⃗Ends .+ b)                      # Right end correction
+    else
+        fLeft  = 0
+        fRight = h * f(b)
     end
-    
-    display(p)
+
+    ∫fdx = fLeft + fInt + fRight                                # Compute corrected integral
+
+    return ∫fdx
 end
---
+++ Research/Gregory/Visuals.jl
@@ -0,0 +1,85 @@
+#=
+# Collection of functions for visuallizing properties of integrators
+#
+# Author: Caleb Jacobs
+# DLM: August 30, 2023
+=#
+
+include("Gregory.jl")
+using Plots
+using BenchmarkTools
+
+"""
+    convergencePlot(a, b, f, ∫f; r = 5, DFT = true, greg = false, nLow = 10, nHigh = 1000, nStep = 1, oLow = 0, oHigh = 10)
+Construct convergence plot of applying DFT or Gregory corrections to `f` over the bounds from `a` to `b`. The true solution to compare to is given by `∫f`. The number of nodes away for DFT is given by `r`.
+"""
+function convergencePlot(a, b, f, ∫f; r = 5, DFT = true, greg = false, nLow = 10, nHigh = 1000, nStep = 1, oLow = 0, oHigh = 10)
+    ns   = [nLow:nStep:nHigh...]                # Number of nodes
+    errsDFT     = zeros(length(ns))             # Initialize DFT errors
+    errsGregory = zeros(length(ns))             # Initialize Gregory errors
+
+    # Begin plotting
+    p = plot(ns[[1,end]], 1 ./ ns[[1,end]], lw = 3, ls = :dash, legend = false)
+    plot!(ns[[1,end]], 1 ./ ns[[1,end]].^2, lw = 3, ls = :dash)
+    plot!(ns[[1,end]], 1 ./ ns[[1,end]].^3, lw = 3, ls = :dash)
+    for N ∈ oLow : oHigh
+        for i ∈ 1 : length(ns)
+            if DFT
+                errsDFT[i] = abs(nonSingDFTInt(f, a, b, ns[i], N, r) - ∫f)
+            end
+
+            if greg
+                errsGregory[i] = abs(nonSingGregoryInt(f, a, b, ns[i], N) - ∫f)
+            end
+        end
+        if DFT
+            plot!(ns, errsDFT, 
+                  yaxis = :log, 
+                  xaxis = :log, 
+                  lw = 2,
+                  lz = N, 
+                  c = :greens,
+                  ylims = (1e-17, 1e0))
+        end
+        if greg
+            plot!(ns, errsGregory, 
+                  yaxis = :log, 
+                  xaxis = :log, 
+                  lw = 2,
+                  lz = N, 
+                  c = :reds,
+                  ylims = (1e-17, 1e0))
+        end
+    end
+    
+    display(p)
+end
+
+"""
+    convergencePlot(a, b, f, α ∫f; r = 5, DFT = true, greg = false, nLow = 10, nHigh = 1000, nStep = 1, oLow = 0, oHigh = 10)
+Construct convergence plot of applying DFT or Gregory corrections to `f` over the bounds from `a` to `b`. The true solution to compare to is given by `∫f`. The number of nodes away for DFT is given by `r`.
+"""
+function convergenceSingPlot(a, b, f, α, ∫f; r = 5, nLow = 10, nHigh = 1000, nStep = 1, oLow = 0, oHigh = 10)
+    ns   = [nLow:nStep:nHigh...]           # Number of nodes
+    errs = zeros(length(ns))               # Initialize DFT errors
+
+    # Begin plotting
+    p = plot( ns[[1,end]], 1 ./ ns[[1,end]],    lw = 3, ls = :dash, legend = false)
+        plot!(ns[[1,end]], 1 ./ ns[[1,end]].^2, lw = 3, ls = :dash)
+        plot!(ns[[1,end]], 1 ./ ns[[1,end]].^3, lw = 3, ls = :dash)
+    for N ∈ oLow : oHigh
+        for i ∈ 1 : length(ns)
+            errs[i] = abs(singDFTInt(f, a, b, ns[i], N, r, α) - ∫f)
+        end
+
+        plot!(ns, errs, 
+              yaxis = :log, 
+              xaxis = :log, 
+              lw = 2,
+              lz = N, 
+              c = :greens,
+              ylims = (1e-16, 1e1))
+    end
+    
+    display(p)
+end
