cbcae1d30f779620a62b82631808df3c71930377
Caleb Jacobs <calebjacobs210@gmail.com>
2023-09-01 13:18:05 -0600

    Started generalized hypergeometric function

--- Research/Gregory/Gregory.jl
+++ Research/Gregory/Gregory.jl
@@ -2,7 +2,7 @@
 # Generalized Gregory Integration
 #
 # Author: Caleb Jacobs
-# DLM: August 31, 2023
+# DLM: September 1, 2023
 =#
 
 using SpecialFunctions
@@ -51,25 +51,6 @@ function nonSingWeights(h, z⃗)
 end
 
 """
-    nonSingInt(f, a, b, n, p)
-Integrate a function `f` from `a` to `b` with no singularities using `n` nodes and order `p`
-"""
-function nonSingGregoryInt(f, a, b, n, p)
-    z⃗ = [range(a, b, length = n)...]                # Trapezoidal nodes
-    h = z⃗[2] - z⃗[1]                                 # Step size
-
-    w = ones(n)                                     # Uncorrected TR weights
-    if p >= 2                                       # Compute corrections if needed
-        c = nonSingWeights(h, z⃗[1 : p - 1] .- a)
-        w[1 : p - 1] += c                           # Correct left weights
-        w[n : -1 : n - p + 2] += c                  # Correct right weights
-    end 
-    w *= h                                          # Rescale weights to step size
-
-    return w ⋅ f.(z⃗)                                # Compute integral
-end
-
-"""
     getNodes(N, r)
 Generate `N` equispaced nodes on a complex circle of radius `r`.
 """
@@ -101,59 +82,6 @@ function getDFTCorrections(α, h, θ, r, N)
     return ω⃗
 end
 
-"""
-    nonSingDFTInt(f, a, b, n, r, N)
-Integrate a function `f` from `a` to `b` with no singularities using `n` internal nodes and `N` correction nodes a radius of `r` away from the base.
-"""
-function nonSingDFTInt(f, a, b, n, N, r)
-    z⃗Int = [range(a, b, length = n)...]         # Internal nodes
-    h = z⃗Int[2] - z⃗Int[1]                       # Internal spacing
-    z⃗End = getNodes(N, r * h)                   # Correction nodes
-
-    fInt   =  h * sum(f.(z⃗Int[2 : end - 1]))    # Internal trapezoidal rule
-
-    if N > 0
-        w = getDFTCorrections(0, h, r*h, N)     # Correction weights
-        fLeft  = h * w ⋅ f.( z⃗End .+ a)         # Left end correction
-        fRight = h * w ⋅ f.(-z⃗End .+ b)         # Right end correction
-    else
-        fLeft  = h * f(a)
-        fRight = h * f(b)
-    end
-
-    ∫fdx = fLeft + fInt + fRight                # Compute corrected integral
-
-    return ∫fdx
-end
-
-"""
-    singDFTInt(f, a, b, n, r, N)
-Integrate a function `f`/(x - `a`)`ᵅ` from `a` to `b` using `n` internal nodes and `N` correction nodes a radius of `r` away from the base.
-"""
-function singDFTInt(f, a, b, n, N, r, α)
-    z⃗Int = [range(a, b, length = n)...]                         # Internal nodes
-    h = z⃗Int[2] - z⃗Int[1]                                       # Internal spacing
-    z⃗Ends = getNodes(N, r * h)                                  # Correction nodes
-
-    sf(x) = f(x) / (x - a)^α                                    # Singular function
-
-    fInt   =  h * sum(sf.(z⃗Int[2 : end - 1]))                   # Internal trapezoidal rule
-
-    if N > 0
-        wL = getDFTCorrections(α, h, r*h, N)                    # Left, singularity correction weights
-        fLeft  = wL ⋅ f.(z⃗Ends .+ a)                            # Left end correction
-
-        wR = getDFTCorrections(0, h, r*h, N)                    # Right correction weights
-        fRight = wR ⋅ sf.(-z⃗Ends .+ b)                          # Right end correction
-    else
-        fLeft  = 0
-        fRight = h * fs(b)
-    end
-
-    ∫fdx = fLeft + fInt + fRight                                # Compute corrected integral
-
-    return ∫fdx
-end
 
 """
     dftInt(f, a, b; α = 0, β = 0, n = 100, N = 20)
@@ -164,6 +92,17 @@ function dftInt(f, a, b; α = 0, β = 0, n = 50, N = 20, r = 5)
     θ = angle(b - a)                                            # Angle of travel for trapezoidal nodes
     z⃗Int  = a .+ h * [1 : n...] * cis(θ)                        # Internal nodes
     z⃗Ends = getNodes(N, r * h)                                  # End correction nodes
+
+    # Ensure singularities are within |α| < 1
+#     tmp = α % 1
+#     αr  = α - tmp
+#     α   = tmp
+# 
+#     tmp = β % 1
+#     βr  = β - tmp
+#     β   = tmp
+
+#     h(x) = (x - a)^αr * (b - x)^βr
 
     # Set internal and endpoint functions
     fi(x) = f(x) / (x - a)^α / (b - x)^β                        # Function to use inside
@@ -193,36 +132,3 @@ function dftInt(f, a, b; α = 0, β = 0, n = 50, N = 20, r = 5)
 
     return ∫f
 end
-# function dftInt(f, a, b; α = 0, β = 0, n = 40, N = 20, r = 5)
-#     z⃗Int  = [range(a, b, length = n + 2)...][2 : end - 1]       # Internal nodes
-#     h     = z⃗Int[2] - z⃗Int[1]                                   # Internal node spacing
-#     z⃗Ends = getNodes(N, r * h)                                  # End correction nodes
-# 
-#     # Set internal and endpoint functions
-#     fi(x) = f(x) / (x - a)^α / (b - x)^β                        # Function to use inside
-#     fa(x) = f(x) / (b - x)^β                                    # Function to use at a
-#     fb(x) = f(x) / (x - a)^α                                    # Function to use at b
-# 
-#     # Compute left end corrections
-#     if N > 0
-#         ωL = getDFTCorrections(α, h, r*h, N)
-#         left∫ = ωL ⋅ fa.(z⃗Ends .+ a)                            # Nth order right correction
-#     else
-#         left∫ = (α == 0 ? h * fa(a) : 0)                        # Zeroth order left correction
-#     end
-# 
-#     # Compute internal trapezoidal rule
-#     int∫ = h * sum(fi.(z⃗Int))
-# 
-#     # Compute right end corrections
-#     if N > 0
-#         ωR = getDFTCorrections(β, h, r*h, N)
-#         right∫ = ωR ⋅ fb.(-z⃗Ends .+ b)                          # Nth order right correction
-#     else
-#         right∫ = (β == 0 ? h * fb(b) : 0)                       # Zeroth order right correction
-#     end
-#         
-#     ∫f = left∫ + int∫ + right∫                                  # Compute total integral
-# 
-#     return ∫f
-# end
--- Research/Gregory/Hypergeometric.jl
+++ Research/Gregory/Hypergeometric.jl
@@ -2,27 +2,89 @@
 # Routines for computing hypergeometric functions
 #
 # Author: Caleb Jacobs
-# DLM: August 31, 2023
+# DLM: September 1, 2023
 =#
 
 using SpecialFunctions
 include("Gregory.jl")
 
 """
-    _2F_1(a, b, c, z)
-Compute the ₂F₁(a, b; c; z) hypergeometric function using DLMF 15.6.1
+    _1F_1(a, b, z)
+Compute the ₁F₁(a; b; z) hypergeometric function.
 """
-function _2F_1(a, b, c, z)
+function _1F_1(a, b, z; n = 40, N = 20)
+    α = 1 - a
+    β = 1 + a - b
+
+    val = dftInt(exp, 0, z, α = α, β = β, n = n, N = N)
+
+    return val / gamma(a) / gamma(b - a) * gamma(b) * z^(1 - b)
+end
+"""
+    _2F_1(a, b, c, z)
+Compute the ₂F₁(a, b; c; z) hypergeometric function.
+"""
+function _2F_1(a, b, c, z; n = 40, N = 20)
     α = 1 - b
     β = 1 + b - c
 
-    if z == 0
-        return dftInt(t -> 1, 0, 1, α = α, β = β)
-    elseif z == 1
-        return Inf + im * Inf
+    f(z) = (1 - z)^(-a)
+
+    val = dftInt(f, 0, z, α = α, β = β, n = n, N = N)
+
+    return val / gamma(b) / gamma(c - b) * gamma(c) * z^(1 - c)
+end
+
+"""
+    pFq(a⃗, b⃗, z)
+Compute the generalized pFq hypergeometric function.
+"""
+# function pFq(a, b, z; n = 40, N = 20, start = true)
+#     if length(a) == 1 && length(b) == 1
+#         return _1F_1(a[1], b[1], z, n = n, N = N)
+#     end
+# 
+#     if length(a) == 2 && length(b) == 1
+#         return _2F_1(a[1], a[2], b[1], z, n = n, N = N)
+#     end
+# 
+# 
+#     α = 1 - a[end]
+#     β = 1 + a[end] - b[end]
+#     f(z) = pFq(a[1 : end - 1], b[1 : end - 1], z)
+#     
+#     val = dftInt(f, 0, z, α = α, β = β, n = n, N = N)
+# 
+#     Γ = gamma(b[end]) /gamma(a[end]) / gamma(b[end] - a[end])
+# 
+#     return Γ * z^(1 - b[end]) * val
+# end
+function pFq(a, b, z; n = 40, N = 20, start = true)
+    ### These are kicking in without factoring in the new α
+    if length(a) == 1 && length(b) == 1
+        return _1F_1(a[1], b[1], z, n = n, N = N)
     end
 
-    val = dftInt(t -> (1 - z * t)^(-a), 0, 1, α = α, β = β)
+    if length(a) == 2 && length(b) == 1
+        return _2F_1(a[1], a[2], b[1], z, n = n, N = N)
+    end
 
-    return val / gamma(b) / gamma(c - b)
+
+    if start
+        α = 1 - a[end]
+    else
+        α = b[end - 1] - a[end]
+    end
+    β = 1 + a[end] - b[end]
+
+    val = dftInt(z -> pFq(a[1 : end - 1], b[1 : end - 1], z, start = false),
+                 0, z, α = α, β = β, n = n, N = N)
+
+    Γ = gamma(b[end]) /gamma(a[end]) / gamma(b[end] - a[end])
+
+    if start
+        return Γ * z^(1 - b[end]) * val
+    else
+        return Γ * val
+    end
 end
