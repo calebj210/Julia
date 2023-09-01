17d888a21096cfbc470d803e3a97d64654300149
Caleb Jacobs <calebjacobs210@gmail.com>
2023-09-01 16:38:42 -0600

    Working non-grid, high order pFq is working except for some branch cut issues.

--- Research/Gregory/Hypergeometric.jl
+++ Research/Gregory/Hypergeometric.jl
@@ -39,6 +39,25 @@ end
     pFq(a⃗, b⃗, z)
 Compute the generalized pFq hypergeometric function.
 """
+function pFq(a, b, z; n = 40, N = 20, start = true)
+    if length(a) == 1 && length(b) == 1
+        return _1F_1(a[1], b[1], z, n = n, N = N)
+    end
+
+    if length(a) == 2 && length(b) == 1
+        return _2F_1(a[1], a[2], b[1], z, n = n, N = N)
+    end
+
+    α = 1 - a[end]
+    β = 1 + a[end] - b[end]
+    f(t) = pFq(a[1 : end - 1], b[1 : end - 1], t * z)
+    
+    val = dftInt(f, 0, 1, α = α, β = β, n = n, N = N)
+
+    Γ = gamma(b[end]) /gamma(a[end]) / gamma(b[end] - a[end])
+
+    return Γ * val
+end
 # function pFq(a, b, z; n = 40, N = 20, start = true)
 #     if length(a) == 1 && length(b) == 1
 #         return _1F_1(a[1], b[1], z, n = n, N = N)
@@ -57,34 +76,44 @@ Compute the generalized pFq hypergeometric function.
 # 
 #     Γ = gamma(b[end]) /gamma(a[end]) / gamma(b[end] - a[end])
 # 
-#     return Γ * z^(1 - b[end]) * val
+#     return Γ * z^(2 - b[end]) * val
 # end
-function pFq(a, b, z; n = 40, N = 20, start = true)
-    ### These are kicking in without factoring in the new α
-    if length(a) == 1 && length(b) == 1
-        return _1F_1(a[1], b[1], z, n = n, N = N)
-    end
-
-    if length(a) == 2 && length(b) == 1
-        return _2F_1(a[1], a[2], b[1], z, n = n, N = N)
-    end
-
-
-    if start
-        α = 1 - a[end]
-    else
-        α = b[end - 1] - a[end]
-    end
-    β = 1 + a[end] - b[end]
-
-    val = dftInt(z -> pFq(a[1 : end - 1], b[1 : end - 1], z, start = false),
-                 0, z, α = α, β = β, n = n, N = N)
-
-    Γ = gamma(b[end]) /gamma(a[end]) / gamma(b[end] - a[end])
-
-    if start
-        return Γ * z^(1 - b[end]) * val
-    else
-        return Γ * val
-    end
-end
+# function pFq(a, b, z; n = 40, N = 20, start = true)
+#     if length(a) == 1 && length(b) == 1
+#         return _1F_1(a[1], b[1], z, n = n, N = N)
+#     end
+# 
+#     if length(a) == 2 && length(b) == 1
+#         return _2F_1(a[1], a[2], b[1], z, n = n, N = N)
+#     end
+# 
+#     α = b[end - 1] - a[end]
+#     β = 1 + a[end] - b[end]
+# 
+#     if abs(α) >= 1
+#         display("α = ")
+#         display(α)
+#     end
+#     if abs(β) >= 1
+#         display("β = ")
+#         display(β)
+#     end
+# 
+#     if β == -1
+#         val = dftInt(x -> (z-x) * pFq(a[1 : end - 1], b[1 : end - 1], x, start = false),
+#                      0, z, α = α, n = n, N = N)
+#     else 
+#         val = dftInt(z -> pFq(a[1 : end - 1], b[1 : end - 1], z, start = false),
+#                      0, z, α = α, β = β, n = n, N = N)
+#     end
+#     display("val = ")
+#     display(val)
+# 
+#     Γ = gamma(b[end]) / gamma(a[end]) / gamma(b[end] - a[end])
+# 
+#     if start
+#         return Γ * z^(1 - b[end]) * val
+#     else
+#         return Γ * val
+#     end
+# end
