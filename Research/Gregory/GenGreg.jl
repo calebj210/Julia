3b2165467ad654f233a3d13a9fdf25ed02e380fa
Caleb Jacobs <calebjacobs210@gmail.com>
2023-09-18 12:10:51 -0600

    Almost finished updated pathing

--- Research/Gregory/GenGreg.jl
+++ Research/Gregory/GenGreg.jl
@@ -8,6 +8,23 @@
 using SpecialFunctions
 include("Grid.jl")
 
+struct Corrections
+    bpu::Vector{ComplexF64}
+    bpd::Vector{ComplexF64}
+    bpl::Vector{ComplexF64}
+    bpr::Vector{ComplexF64}
+
+    cru::Vector{ComplexF64}
+    crd::Vector{ComplexF64}
+    crl::Vector{ComplexF64}
+    crr::Vector{ComplexF64}
+
+    epu::Vector{ComplexF64}
+    epd::Vector{ComplexF64}
+    epl::Vector{ComplexF64}
+    epr::Vector{ComplexF64}
+end
+
 " Generate vandermond type matrix from nodes in the grid `g` with indices `idx`."
 function getVand(idx, g::Grid)
     A = [zk^i for i ∈ 0 : length(idx) - 1, zk ∈ g.z[idx]]
@@ -40,29 +57,133 @@ end
 
 Compute end correction weights for ``∫₀ᶻ(u)ᵅ(z-u)ᵝf(u)du`` with singularity of order `α` with points on the grid `g` about the square radius `r`.
 """
-function getCorrectionWeights(r, g::Grid, α)
-    idx = getCorrectionIndices(g.c, r, g)           # Correction indices about the origin
+function getCorrectionWeights(r, g::Grid; α = 0, dir = 1 + 0im)
+    idx = getCorrectionIndices(g.c, g)                      # Correction indices about the origin
 
-    N = length(idx)                                 # Number of correction nodes
+    N = length(idx)                                         # Number of correction nodes
 
-    A = getVand(idx, g)                             # Left hand side
+    A = getVand(idx, g)                                     # Left hand side
 
-    b = [-zeta(-α - k) * g.h^k for k ∈ 0 : N - 1]   # Right hand side
+    b = [-zeta(-α - k) * (dir * g.h)^k for k ∈ 0 : N - 1]   # Right hand side
 
-    ω = A \ b                                       # Solve for weights
+    ω = A \ b                                               # Solve for weights
 
-    ω *=  g.h^(1 + α)                               # Scale weights by grid spacing factor
+    ω *=  (dir * g.h)^(1 + α)                               # Scale weights by grid spacing factor
 
     return ω
 end
 
 """
-    populateRow!(row, idx, path, g::Grid)
+    getCorrections(g; α = 0, β = 0)
 
-Populate differentiation entries of `row` corresponding to `g`.z[`idx`] following a `path`.
+Compute all possible end/corner corrections over a grid `g`.
 """
-function populateRow!(row, idx, path, g::Grid)
+function getCorrections(g::Grid; α = 0, β = 0)
+    # Basepoint corrections
+    bpu = getCorrectionWeights(g.np, g, α = α, dir =  0 + 1im)  # Up
+    bpd = getCorrectionWeights(g.np, g, α = α, dir =  0 - 1im)  # Down
+    bpl = getCorrectionWeights(g.np, g, α = α, dir = -1 + 0im)  # Left
+    bpr = getCorrectionWeights(g.np, g, α = α, dir =  1 + 0im)  # Right
+
+    # Corner corrections
+    cru = getCorrectionWeights(g.np, g, dir =  0 + 1im)         # Up
+    crd = getCorrectionWeights(g.np, g, dir =  0 - 1im)         # Down
+    crl = getCorrectionWeights(g.np, g, dir = -1 + 0im)         # Left
+    crr = getCorrectionWeights(g.np, g, dir =  1 + 0im)         # Right
+
+    # Endpoint corrections
+    epu = getCorrectionWeights(g.np, g, α = β, dir =  0 + 1im)  # Up
+    epd = getCorrectionWeights(g.np, g, α = β, dir =  0 - 1im)  # Down
+    epl = getCorrectionWeights(g.np, g, α = β, dir = -1 + 0im)  # Left
+    epr = getCorrectionWeights(g.np, g, α = β, dir =  1 + 0im)  # Right
+
+    return Corrections(bpu, bpd, bpl, bpr, cru, crd, crl, crr, epu, epd, epl, epr)
+end
+
+function getCorrection(c::Corrections, dir, type::String)
+    if type == "cr"
+        if dir == 0.0 + 1im
+            return c.cru
+        elseif dir == 0.0 - 1im
+            return c.crd
+        elseif dir == -1.0 - 0im
+            return c.crl
+        elseif dir == 1.0 - 0im
+            return c.crr
+        else
+            throw(DomainError(dir, "not a valid direction of grid integration"))
+        end
+    elseif type == "bp"
+        if dir == 0.0 + 1im
+            return c.bpu
+        elseif dir == 0.0 - 1im
+            return c.bpd
+        elseif dir == -1.0 - 0im
+            return c.bpl
+        elseif dir == 1.0 - 0im
+            return c.bpr
+        else
+            throw(DomainError(dir, "not a valid direction of grid integration"))
+        end
     
+    elseif type == "ep"
+        if dir == 0.0 + 1im
+            return c.epu
+        elseif dir == 0.0 - 1im
+            return c.epd
+        elseif dir == -1.0 - 0im
+            return c.epl
+        elseif dir == 1.0 - 0im
+            return c.epr
+        else
+            throw(DomainError(dir, "not a valid direction of grid integration"))
+        end
+        
+    else
+        throw(DomainError(type, "needs a valid point type of bp, cr, or ep."))
+    end
+end
+
+"""
+    populateExternalRow!(row, idx, path, g::Grid, α, β)
+
+Populate differentiation entries of `row` corresponding to `g`.z[`zIdx`].
+"""
+function populateExternalRow!(row, zIdx, g::Grid, α, β)
+    path = getPath(zIdx, g, g.np)                                   # Get path from origin to node
+    N = length(path)                                                # Number of paths to travel
+    c = getCorrections(g, α = α, β = β)                             # End/corner corrections
+
+    for (n, p) ∈ pairs(path)
+        dir = sign(p.f - p.i)                                       # Compute direction of travel
+
+        αβt = g.z[p.p].^α .* (g.z[zIdx] .- g.z[p.p]).^β             # Trapezoidal αβ factor
+
+        row[p.p] .= dir * g.h * αβt                                 # Compute trapezoidal weights
+
+        iIdx = getCorrectionIndices(p.i, g)                         # Initial point correction indices
+        fIdx = getCorrectionIndices(p.f, g)                         # Final point correction indices
+
+        if n == 1
+            βt = (g.z[zIdx] .- g.z[iIdx]).^β                        # Basepoint β factor
+
+            row[iIdx] +=  getCorrection(c, dir, "bp") .* βt         # Basepoint corrections
+        else
+            αβt = g.z[iIdx].^α .* (g.z[zIdx] .- g.z[iIdx]).^β       # Corner αβ factor
+
+            row[iIdx] +=  getCorrection(c, dir, "cr") .* αβt        # Corner corrections
+        end
+
+        if n == N
+            αt = g.z[fIdx].^α                                       # Endpoint α factor
+            
+            row[fIdx] += getCorrection(c, -dir, "ep") .* αt         # Endpoint corrections
+        else
+            αβt = g.z[fIdx].^α .* (g.z[zIdx] .- g.z[fIdx]).^β       # Corner αβ factor
+
+            row[fIdx] += getCorrection(c, -dir, "cr") .* αβt        # Corner corrections
+        end
+    end
 end
 
 """
@@ -73,9 +194,9 @@ Generate differentiation matrix for ``∫₀ᶻ(u)ᵅ(z-u)ᵝf(u)du`` over a gri
 The radius to use the Taylor expansion is given by `ir` while the relative radius of the correction stencils are given by `er`.
 """
 function getDiffMat(n, r; α = 0.0, β = 0.0, ir = 0.5, er = 3)
-    g = getGrid(n, r, ir = ir, p = er)                      # Generate grid
+    g = getGrid(n, r, ir = ir, p = er)                                  # Generate grid
 
-    (iMap, eMap) = getReducedGridMap(g)                     # Index maps to reduced grid for indexing through diff matrix
+    (iMap, eMap) = getReducedGridMap(g)                                 # Index maps to reduced grid for indexing through diff matrix
 
     D = zeros(ComplexF64, length(g.i) + length(g.e), length(g.z))       # Initialize differentiation matrix
 
@@ -84,79 +205,87 @@ function getDiffMat(n, r; α = 0.0, β = 0.0, ir = 0.5, er = 3)
         D[iMap[i], g.i] = getInternalWeights(iIdx, g, α, β)
     end
 
-    # Populate external weights using trapezoidal rule and end corrections
-    bpω = getCorrectionWeights(er, g, α)                        # Base point correction
-    crω = getCorrectionWeights(er, g, 0.0)                      # Corner correction
-    epω = getCorrectionWeights(er, g, β)                        # End point correction
+    # Populate external weights using generalized Gregory quadrature
+    for (e, eIdx) ∈ pairs(g.e)
+        populateExternalRow!(D[eMap[e], :], eIdx, g, α, β)
+    end
 
-    for (i, eIdx) ∈ pairs(g.e)
-        z = g.z[eIdx]                                           # Current z value
+    return D
+end
 
-        # Populate trapezoidal weights
-        (hIdx, vIdx, cIdx) = getPath(eIdx, g)                   # Indices along path of integration
-
-        αβt = g.z[hIdx].^α .* (z .- g.z[hIdx]).^β               # Horizontal α-β term in integrand
-        if real(z) > 0
-            D[eMap[i], hIdx] .=  g.h * αβt                      # Set trapezoidal weights moving right
-        else
-            D[eMap[i], hIdx] .= -g.h * αβt                      # Set trapezoidal weights moving left
-        end
-
-        αβt = g.z[vIdx].^α .* (z .- g.z[vIdx]).^β               # Vertical α-β term in integrand
-        if imag(z) > 0
-            D[eMap[i], vIdx] .=  im * g.h * αβt                 # Set trapezoidal weights moving up
-        else
-            D[eMap[i], vIdx] .= -im * g.h * αβt                 # Set trapezoidal weights moving down
-        end
-
-        # Populate left-right end correction weights
-        bpIdx = getCorrectionIndices(g.c,  er, g)               # Base point correction indices
-        crIdx = getCorrectionIndices(cIdx, er, g)               # Corner correction indices
-        epIdx = getCorrectionIndices(eIdx, er, g)               # Endpoint correction indices
-        
-        if imag(g.z[eIdx]) != 0
-            if imag(z) > 0
-                rbpIdx = rotCorrection(bpIdx, 1)                # Rotated base point indices
-                rcrIdx = rotCorrection(crIdx, 3)                # Rotated base point indices
-
-                hbp = im^(1 + α)                                # Base point angle correction
-                hcr = im                                        # Corner  angle correction
-            else 
-                rbpIdx = rotCorrection(bpIdx, 3)                # Rotated base point indices
-                rcrIdx = rotCorrection(crIdx, 1)                # Rotated base point indices
-
-                hbp = (-im)^(1 + α)                             # Base point angle correction
-                hcr = (-im)                                     # Corner  angle correction
-            end
-
-            βt  = (z .- g.z[rbpIdx]).^β                         # Base point β term in integrand
-            αβt = g.z[rcrIdx].^α .* (z .- g.z[rcrIdx]).^β       # Corner α-β term in integrand
-
-            D[eMap[i], rbpIdx] += hbp * bpω .* βt               # Basepoint correction
-            D[eMap[i], rcrIdx] += hcr * crω .* αβt              # Corner left-right correction
-        end
-
-        if real(g.z[eIdx]) != 0
-            if real(z) > 0
-                rcrIdx = rotCorrection(crIdx, 0)                # Rotated corner indices
-                repIdx = rotCorrection(epIdx, 2)                # Rotated endpoint indices
-
-                hcr = 1.0                                       # Corner angle correction
-                hep = 1.0^(1 + β)                               # Endpoint angle correction
-            else
-                rcrIdx = rotCorrection(crIdx, 2)                # Rotated corner indices
-                repIdx = rotCorrection(epIdx, 0)                # Rotated endpoint indices
-
-                hcr = (-1 + 0im)                                # Corner angle correction
-                hep = (-1 + 0im)^(1 + β)                        # Endpoint angle correction
-            end
-
-            αβt = g.z[rcrIdx].^α .* (z .- g.z[rcrIdx]).^β       # Corner α-β term in integrand
-            αt  = g.z[repIdx].^α                                # Endpoint α term in integrand
-
-            D[eMap[i], rcrIdx] += hcr * crω .* αβt              # Corner up-down correction
-            D[eMap[i], repIdx] += hep * epω .* αt               # Endpoint correction
-        end
+#     # Populate external weights using trapezoidal rule and end corrections
+#     bpω = getCorrectionWeights(er, g, α)                        # Base point correction
+#     crω = getCorrectionWeights(er, g, 0.0)                      # Corner correction
+#     epω = getCorrectionWeights(er, g, β)                        # End point correction
+# 
+#     for (i, eIdx) ∈ pairs(g.e)
+#         z = g.z[eIdx]                                           # Current z value
+# 
+#         # Populate trapezoidal weights
+#         (hIdx, vIdx, cIdx) = getPath(eIdx, g)                   # Indices along path of integration
+# 
+#         αβt = g.z[hIdx].^α .* (z .- g.z[hIdx]).^β               # Horizontal α-β term in integrand
+#         if real(z) > 0
+#             D[eMap[i], hIdx] .=  g.h * αβt                      # Set trapezoidal weights moving right
+#         else
+#             D[eMap[i], hIdx] .= -g.h * αβt                      # Set trapezoidal weights moving left
+#         end
+# 
+#         αβt = g.z[vIdx].^α .* (z .- g.z[vIdx]).^β               # Vertical α-β term in integrand
+#         if imag(z) > 0
+#             D[eMap[i], vIdx] .=  im * g.h * αβt                 # Set trapezoidal weights moving up
+#         else
+#             D[eMap[i], vIdx] .= -im * g.h * αβt                 # Set trapezoidal weights moving down
+#         end
+# 
+#         # Populate left-right end correction weights
+#         bpIdx = getCorrectionIndices(g.c,  er, g)               # Base point correction indices
+#         crIdx = getCorrectionIndices(cIdx, er, g)               # Corner correction indices
+#         epIdx = getCorrectionIndices(eIdx, er, g)               # Endpoint correction indices
+#         
+#         if imag(g.z[eIdx]) != 0
+#             if imag(z) > 0
+#                 rbpIdx = rotCorrection(bpIdx, 1)                # Rotated base point indices
+#                 rcrIdx = rotCorrection(crIdx, 3)                # Rotated base point indices
+# 
+#                 hbp = im^(1 + α)                                # Base point angle correction
+#                 hcr = im                                        # Corner  angle correction
+#             else 
+#                 rbpIdx = rotCorrection(bpIdx, 3)                # Rotated base point indices
+#                 rcrIdx = rotCorrection(crIdx, 1)                # Rotated base point indices
+# 
+#                 hbp = (-im)^(1 + α)                             # Base point angle correction
+#                 hcr = (-im)                                     # Corner  angle correction
+#             end
+# 
+#             βt  = (z .- g.z[rbpIdx]).^β                         # Base point β term in integrand
+#             αβt = g.z[rcrIdx].^α .* (z .- g.z[rcrIdx]).^β       # Corner α-β term in integrand
+# 
+#             D[eMap[i], rbpIdx] += hbp * bpω .* βt               # Basepoint correction
+#             D[eMap[i], rcrIdx] += hcr * crω .* αβt              # Corner left-right correction
+#         end
+# 
+#         if real(g.z[eIdx]) != 0
+#             if real(z) > 0
+#                 rcrIdx = rotCorrection(crIdx, 0)                # Rotated corner indices
+#                 repIdx = rotCorrection(epIdx, 2)                # Rotated endpoint indices
+# 
+#                 hcr = 1.0                                       # Corner angle correction
+#                 hep = 1.0^(1 + β)                               # Endpoint angle correction
+#             else
+#                 rcrIdx = rotCorrection(crIdx, 2)                # Rotated corner indices
+#                 repIdx = rotCorrection(epIdx, 0)                # Rotated endpoint indices
+# 
+#                 hcr = (-1 + 0im)                                # Corner angle correction
+#                 hep = (-1 + 0im)^(1 + β)                        # Endpoint angle correction
+#             end
+# 
+#             αβt = g.z[rcrIdx].^α .* (z .- g.z[rcrIdx]).^β       # Corner α-β term in integrand
+#             αt  = g.z[repIdx].^α                                # Endpoint α term in integrand
+# 
+#             D[eMap[i], rcrIdx] += hcr * crω .* αβt              # Corner up-down correction
+#             D[eMap[i], repIdx] += hep * epω .* αt               # Endpoint correction
+#         end
 
 #         if real(z) != 0
 #             if real(z) > 0
@@ -201,7 +330,7 @@ function getDiffMat(n, r; α = 0.0, β = 0.0, ir = 0.5, er = 3)
 #             D[eMap[i], rcrIdx] += hcr * crω .* αβt              # Corner up-down correction
 #             D[eMap[i], repIdx] += hep * epω .* αt               # Endpoint correction
 #         end
-    end
-
-    return D
-end
+#     end
+# 
+#     return D
+# end
--- Research/Gregory/Grid.jl
+++ Research/Gregory/Grid.jl
@@ -36,6 +36,15 @@ struct Grid
 
     "Radius of square grid"
     r::Float64            
+
+    "Number of padded nodes"
+    np::Int64
+end
+
+struct Path
+    i::Int64
+    p::Vector{Int64}
+    f::Int64
 end
 
 """
@@ -49,6 +58,8 @@ function getGrid(n, r; ir = 0.5, p = 0)::Grid
     x⃗ = [range(0, r, length = n + 1)...]                # real parts
     y⃗ = [range(0, r, length = n + 1)...]                # imaginary parts
     h  = abs(x⃗[2] - x⃗[1])                               # Grid spacing
+
+    np = p                                              # Number of padding nodes
 
     pad = r .+ h * [1:p...]                             # Padded nodes
     xp = [-pad[end : -1 : 1]; -x⃗[end: -1: 2]; x⃗; pad]   # Padded x vector
@@ -80,7 +91,7 @@ function getGrid(n, r; ir = 0.5, p = 0)::Grid
         end
     end
 
-    return Grid(z⃗, i, e, p, t, dx, dy, c, h, r)
+    return Grid(z⃗, i, e, p, t, dx, dy, c, h, r, np)
 end
 
 """
@@ -141,40 +152,40 @@ function getIdx(z::ComplexF64, g::Grid)
     end
 end
 
-""" 
-    getPath(zIdx, g)
-
-Get indices to traverse the complex grid from 0 to z with index `zIdx`. The indices are returned as a tuple (horizontal, vertical)
-"""
-function getPath(zIdx::Int64, g::Grid)
-    Nx = round(Int64, real(g.z[zIdx]) / g.h)                        # Number of horizontal nodes
-    Ny = round(Int64, imag(g.z[zIdx]) / g.h)                        # Number of vertical nodes
-    
-    if Nx != 0
+# """ 
+#     getPath(zIdx, g)
+# 
+# Get indices to traverse the complex grid from 0 to z with index `zIdx`. The indices are returned as a tuple (horizontal, vertical)
+# """
+# function getPath(zIdx::Int64, g::Grid)
+#     Nx = round(Int64, real(g.z[zIdx]) / g.h)                        # Number of horizontal nodes
+#     Ny = round(Int64, imag(g.z[zIdx]) / g.h)                        # Number of vertical nodes
+#     
+#     if Nx != 0
 #         horiz = g.c .+ 
 #                 sign(Nx) * g.dx * [1 : abs(Nx) - 1...]              # March horizontally to z ignoring endpoint
-        horiz = g.c + 
-                Ny * g.dy .+ 
-                sign(Nx) * g.dx * [1 : abs(Nx) - 1...]              # March horizontally to z ignoring endpoint
-    else
-        horiz = []                                                  # No horizontal nodes
-    end
-
-    if Ny != 0
+#         horiz = g.c + 
+#                 Ny * g.dy .+ 
+#                 sign(Nx) * g.dx * [1 : abs(Nx) - 1...]              # March horizontally to z ignoring endpoint
+#     else
+#         horiz = []                                                  # No horizontal nodes
+#     end
+# 
+#     if Ny != 0
 #         vert = g.c + 
 #                Nx * g.dx .+
 #                sign(Ny) * g.dy * [1 : abs(Ny) - 1...]               # March vertically to z ignoring endpoints
-        vert = g.c .+ 
-               sign(Ny) * g.dy * [1 : abs(Ny) - 1...]               # March vertically to z ignoring endpoints
-    else
-        vert = []                                                   # No vertical nodes
-    end
-
-    corner = g.c + Ny * g.dy                                        # Index of corner if up then right
+#         vert = g.c .+ 
+#                sign(Ny) * g.dy * [1 : abs(Ny) - 1...]               # March vertically to z ignoring endpoints
+#     else
+#         vert = []                                                   # No vertical nodes
+#     end
+# 
+#     corner = g.c + Ny * g.dy                                        # Index of corner if up then right
 #     corner = g.c + Nx * g.dx                                        # Index of corner if right then up
-
-    return (horiz, vert, corner)
-end
+# 
+#     return (horiz, vert, corner)
+# end
 
 function getPath(zIdx::Int64, g::Grid, r)
     Nx = round(Int64, real(g.z[zIdx]) / g.h)                        # Number of horizontal nodes
@@ -188,61 +199,89 @@ function getPath(zIdx::Int64, g::Grid, r)
             v = g.c .+ sgn(Ny) * g.dy * [1 : abs(Ny) - 1...]        # Move vertically
 
             c = v[end] + g.dy * sgn(Ny)                             # Corner index
+
+            p1 = Path(g.c, v, c)
             
             h = c .+ g.dx * [1 : Nx - 1...]                         # Move horizontally
+
+            p2 = Path(c, h, zIdx)
             
-            path = [v, c, h]
+            path = [p1, p2]
         else                                                        # U contour
             v1 = g.c .+ sgn(Ny) * g.dy * [1 : r + abs(Ny) - 1...]   # Move past point vertically
 
             c1 = v1[end] + g.dy * sgn(Ny)                           # First corner
 
+            p1 = Path(g.c, v1, c1)
+
             h  = c1 .+ g.dx * [1 : Nx - 1...]                       # Move horizontally
 
             c2 = h[end] + g.dx                                      # Second corner
 
+            p2 = Path(c1, h, c2)
+
             v2 = c2 .- g.dy * [1 : r - 1...]                        # Move vertically back to point
 
-            path = [v1, c1, h, c2, v2]           
+            p3 = Path(c2, v2, zIdx)
+
+            path = [p1, p2, p3]           
         end
     elseif abs(Nx) <= r                                             # Vertical band case
         h1 = g.c .- g.dx * [1 : r + abs(Nx) - 1...]                 # Move past point horizontally
 
         c1 = h1[end] - g.dx                                         # First corner
 
+        p1 = Path(g.c, h1, c1)
+
         v  = c1 .+ g.dy * sgn(Ny) * [1 : abs(Ny) - 1...]            # Move vertically
 
         c2 = v[end] + g.dy * sgn(Ny)                                # Second corner
 
-        h2 = Nx <= 0 ? c2 .+ g.dx * [1 : r +  abs(Nx) - 1...] :     # Move horizontally back to point
+        p2 = Path(c1, v, c2)
+
+        h2 = Nx <= 0 ? c2 .+ g.dx * [1 : r - 1...] :                # Move horizontally back to point
                        c2 .+ g.dx * [1 : r + 2abs(Nx) - 1...]
+
+        p3 = Path(c2, h2, zIdx)
         
-        path = [h1, c1, v, c2, h2]
+        path = [p1, p2, p3]
     elseif Nx < -r                                                  # Far left side cases
         if abs(Ny) > r                                              # Branch-cut-following L contour
             h = g.c .- g.dx * [1 : abs(Nx) - 1...]                  # Move left horizontally
 
             c = h[end] - g.dx                                       # Corner
 
+            p1 = Path(g.c, h, c)
+
             v = c .+ g.dy * sgn(Ny) * [1 : abs(Ny) - 1...]          # Move vertically to end point
 
-            path = [h, c, v]
+            p2 = Path(c, v, zIdx)
+
+            path = [p1, p2]
         else                                                        # Branch-cut-following U contour
             h1 = g.c .- g.dx * [1 : r + abs(Nx) - 1...]
 
             c1 = h1[end] - g.dx
 
+            p1 = Path(g.c, h1, c1)
+
             v1 = c1 .+ g.dy * sgn(Ny) * [1 : 2r - 1...]
 
             c2 = v1[end] + g.dy * sgn(Ny)
 
+            p2 = Path(c1, v1, c2)
+
             h2 = c2 .+ g.dx * [1 : r - 1...]
 
-            c3 = v2[end] + g.dx
+            c3 = h2[end] + g.dx
+
+            p3 = Path(c2, h2, c3)
 
             v2 = c3 .- g.dy * sgn(Ny) * [1 : 2r - abs(Ny) - 1...]
 
-            path = [h1, c1, v1, c2, h2, c3, v2]
+            p4 = Path(c3, v2, zIdx)
+
+            path = [p1, p2, p3, p4]
         end
     end
 
@@ -257,7 +296,9 @@ end
     getCorrectionIndices(z, r, z⃗)
 Get indices of surrounding correction nodes in z⃗ an adjacent distance of r away from z.
 """
-function getCorrectionIndices(zIdx::Int64, r::Int64, g::Grid)
+function getCorrectionIndices(zIdx::Int64, g::Grid)
+    r = g.np                                                            # Radius of padding nodes
+
     if r == 0
         return [zIdx]
     end
